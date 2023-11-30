8fd99035c32e3ad5ca4b8d8a3187c8f416f5723a
Diana Kolganova <glebova_diana@bk.ru>
2023-11-30 23:29:17 +0300

    * add custom login page

--- src/main/java/com/kolganova/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/com/kolganova/firstsecurityapp/config/SecurityConfig.java
@@ -1,20 +1,41 @@
 package com.kolganova.firstsecurityapp.config;
 
-import com.kolganova.firstsecurityapp.services.PersonDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private PersonDetailsService personDetailsService;
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+    // manage authentication
+    @Bean
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
+    // conf SpringSecurity itself + conf authorization
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                //authorization
+                .authorizeRequests()
+                .requestMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                //login
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+
+        return http.build();
     }
 
     @Bean
@@ -23,6 +44,3 @@ public class SecurityConfig {
     }
 
 }
-
-
-
