2695a78b2f479a76e4fbb8147653fea3c812f20e
Diana Kolganova <glebova_diana@bk.ru>
2023-12-01 17:16:25 +0300

    * mod csrf

--- src/main/java/com/kolganova/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/com/kolganova/firstsecurityapp/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,7 +33,7 @@ public class SecurityConfig {
     // conf SpringSecurity itself + conf authorization
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
                 //authorization
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/auth/login", "/error", "/auth/registration").permitAll()
